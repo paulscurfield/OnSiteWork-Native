150c3a87040ac371d50a975e1b0c76febfb7fe6f
paulscurfield <paulscurfield@gmail.com>
2026-07-18 22:38:04 +1000

    Add shared UI component typing

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -2,49 +2,63 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef(({ className, ...props }, ref) => (
+/** @typedef {React.HTMLAttributes<HTMLDivElement>} CardProps */
+
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardRender = ({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}
     {...props} />
-))
+)
+const Card = React.forwardRef(CardRender)
 Card.displayName = "Card"
 
-const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardHeaderRender = ({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props} />
-))
+)
+const CardHeader = React.forwardRef(CardHeaderRender)
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardTitleRender = ({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("font-semibold leading-none tracking-tight", className)}
     {...props} />
-))
+)
+const CardTitle = React.forwardRef(CardTitleRender)
 CardTitle.displayName = "CardTitle"
 
-const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardDescriptionRender = ({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props} />
-))
+)
+const CardDescription = React.forwardRef(CardDescriptionRender)
 CardDescription.displayName = "CardDescription"
 
-const CardContent = React.forwardRef(({ className, ...props }, ref) => (
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardContentRender = ({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
+)
+const CardContent = React.forwardRef(CardContentRender)
 CardContent.displayName = "CardContent"
 
-const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, CardProps>} */
+const CardFooterRender = ({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex items-center p-6 pt-0", className)}
     {...props} />
-))
+)
+const CardFooter = React.forwardRef(CardFooterRender)
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
